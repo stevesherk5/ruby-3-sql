55aeecc468707823db24944fc87962f9efd30cd0
Steve Sherk <steven.sherk@kellogg.northwestern.edu>
2024-01-30 17:32:34 +0000

    Finished class 1.30

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -9,9 +9,39 @@ Contact.destroy_all
 # **************************
 
 # - Insert and read contact data for companies in the database
+apple = Company.find_by({"name"=>"Apple"})
+amazon = Company.find_by({"name"=>"Amazon"})
 
 # 1. insert new rows in the contacts table with relationship to a company
+new_contact = Contact.new
+new_contact["first_name"] = "Tim"
+new_contact["last_name"] = "Cook"
+new_contact["e-mail"] = "Tim@apple.com"
+new_contact["company_id"] = apple["id"]
+new_contact.save
+
+new_contact = Contact.new
+new_contact["first_name"] = "Jeff"
+new_contact["last_name"] = "Bezons"
+new_contact["e-mail"] = "jeff@amazon.com"
+new_contact["company_id"] = amazon["id"]
+new_contact.save
+
+new_contact = Contact.new
+new_contact["first_name"] = "Bob"
+new_contact["last_name"] = "Jones"
+new_contact["e-mail"] = "bob@apple.com"
+new_contact["company_id"] = apple["id"]
+new_contact.save
 
 # 2. How many contacts work at Apple?
+apple_contacts = Contact.where({"company_id"=>apple["id"]})
+puts "Apple contacts: #{apple_contacts.count}"
 
+amazon_contacts = Contact.where({"company_id"=>amazon["id"]})
+puts "Amazon contacts: #{amazon_contacts.count}"
 # 3. What is the full name of each contact who works at Apple?
+
+for contact in apple_contacts
+    puts "#{contact["first_name"]} #{contact["last_name"]}"
+end
--- labs/2-models.rb
+++ labs/2-models.rb
@@ -21,15 +21,46 @@ Salesperson.destroy_all
 
 # 2. insert 1-2 rows in salespeople table.
 
+salesguy = Salesperson.new
+salesguy["first_name"] = "Steve"
+salesguy["last_name"] = "Sherk"
+salesguy["e-mail"] = "steve@gmail.com"
+salesguy.save
+
+salesguy = Salesperson.new
+salesguy["first_name"] = "Sofia"
+salesguy["last_name"] = "Gonzo"
+salesguy["e-mail"] = "sof@gmail.com"
+salesguy.save
+
+
 # 3. write code to display how many salespeople rows are in the database
+puts "There are #{Salesperson.all.count} salespeople"
 
 # ---------------------------------
 # Salespeople: 2
 
 # 4. modify/update column data for a row in the salespeople table.
+steve = Salesperson.find_by({"first_name"=>"Steve"})
+steve["phone_number"] = 9376231995
+steve.save
 
 # CHALLENGE:
 # 5. write code to display each salesperson's full name
+
+# sofia = Salesperson.find_by({"first_name"=>"Sofia"})
+
+# puts "#{steve["first_name"]} #{steve["last_name"]}"
+# puts "#{sofia["first_name"]} #{sofia["last_name"]}"
+
+salespeople = Salesperson.all
+puts salespeople.inspect 
+
+for salesperson in salespeople 
+    puts "#{salesperson["first_name"]} #{salesperson["last_name"]}"
+end
+
+
 
 # ---------------------------------
 # Salespeople: 2
